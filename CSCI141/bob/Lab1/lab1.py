7658e45e6658e7a7b54eb163ee3b9e1e006a2ec7
clay-l3 <clayl3@wwu.edu>
2025-01-14 08:42:48 -0800

    Upload lab1

---
+++ CSCI141/bob/Lab1/lab1.py
@@ -0,0 +1,8 @@
+#Author: Lincoln Clay
+#Date: January 14, 2025
+#Description: Code for Lab1, CSCI
+print("Hello World!")
+name = input("What is your name? ")
+name_length = len(name)
+print("Hello", name)
+print("You have", name_length, "characters in your name.")
